518575169f0cecf213e34a646873fc3b749a768f
AleksandarP91 <Pejcinovski91@gmail.com>
2016-02-22 14:17:21 +0100

    uppload

---
+++ Server/SocketServerC/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketServerC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server server = new Server();
+            server.SocketServer("10.42.104.47", 8145);
+
+        }
+    }
+}
---
+++ Server/SocketServerC/Server.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.IO.Ports;
+
+namespace SocketServerC
+{
+    class Server
+    {
+        static UnicodeEncoding encoding = new UnicodeEncoding();
+        static Socket listeningSocket = null;
+        static Socket socket = null;
+        const Int32 BUFFERLENGTH = 40;
+
+        public void SocketServer(string ip, int port)
+        {
+            IPAddress ipAddress = IPAddress.Parse(ip);
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, port);
+            SerialPort usbport = new SerialPort();
+
+            usbport.PortName = "COM6";
+            usbport.BaudRate = 9600;
+
+            
+
+                try
+                {
+                    listeningSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    listeningSocket.Bind(localEndPoint);
+                    listeningSocket.Listen(1);
+
+                    //Found a connection
+                    socket = listeningSocket.Accept();
+
+
+                    Byte[] buffer = new Byte[40];
+                    socket.Receive(buffer);
+
+                    String message = encoding.GetString(buffer);
+
+
+
+                    usbport.Open();
+                    if (usbport.IsOpen)
+                    {
+                        usbport.Write(message);
+                    }
+                    usbport.Close();
+
+                    Console.ReadLine();
+
+
+
+
+
+                }
+
+                catch (Exception exception)
+                {
+                    Console.WriteLine("ERROR: " + exception.Message + "\n" + exception.StackTrace);
+                    Console.ReadLine();
+                }
+                finally
+
+                {
+                    if (listeningSocket != null)
+                        listeningSocket.Close();
+                    if (socket != null)
+                        socket.Close();
+
+
+                }
+
+
+            }
+        }
+    }
+
+
